a16bbe742ac5d6d91f1ff9241788c32d3aa4862c
handakim25 <handakim25@gmail.com>
2023-12-11 12:51:59 +0900

    Feat: Tile 수정 기능을 추가
    Tile을 설정하고 움직일 수 있는 기능을 추가
    Tile의 정보를 얻을 수 있는 기능을 추가
    Tile의 수정하고 위치를 수정하는 기능을 추가

--- Assets/Scripts/RollGame/Tile/TileMap.cs
+++ Assets/Scripts/RollGame/Tile/TileMap.cs
@@ -18,10 +18,18 @@ namespace Zephyr.RollGame.Tile
         /// </summary>
         private readonly Transform _parent;
         /// <summary>
-        /// TileMap에 생성된 Tile들
+        /// TileMap에 생성된 Tile들, 좌하단 (0, 0), ~ (x, y) 좌표계.
+        /// 비어있는 Tile은 null로 표시한다.
         /// </summary>
         private GameObject[,] _tiles;
         private Dictionary<GameObject, TileData> _tileDataMap = new();
+        public int Width => _tiles?.GetLength(0) ?? 0;
+        public int Height => _tiles?.GetLength(1) ?? 0;
+        public bool IsInTileMap(int x, int y) =>
+             x >= 0 && x < Width && y >= 0 && y < Height;
+
+        private int _startX = 0;
+        private int _startY = 0;
 
         private TilePalette _tilePalette;
         private const string _defaultTilePalettePath = "RollGame/DefaultTilePalette";
@@ -123,24 +131,36 @@ namespace Zephyr.RollGame.Tile
             }
             _tiles = new GameObject[stage.Width, stage.Height];
 
-            int startX = -(stage.Width / 2);
-            int startY = -(stage.Height / 2);
+            _startX = -(stage.Width / 2);
+            _startY = -(stage.Height / 2);
 
             for(int y = 0; y < stage.Height; ++y)
             {
                 for(int x = 0; x < stage.Width; ++x)
                 {
                     TileData tileData = GetStageTileData(stage, x, y);
-                    GameObject tile = CreateTile(startX + x, startY + y, tileData, $"Tile_{x}_{y}");
-                    if(tile == null)
-                    {
-                        return; 
-                    }
-                    _tiles[x, y] = tile;
-                    _tileDataMap.Add(tile, tileData);
-                    _tileDataMap.TryAdd(tile, tileData);
+                    GameObject tile = CreateTile(_startX + x, _startY + y, tileData, $"Tile_{x}_{y}");
+                    AddTile(x, y, tile, tileData);
                 }
             }
+        }
+
+        /// <summary>
+        /// Stage의 TileMap에서 해당 좌표의 TileData를 가져온다.
+        /// </summary>
+        /// <param name="stage">Stage Data</param>
+        /// <param name="x">Stage X 좌표</param>
+        /// <param name="y">Stage Y 좌표</param>
+        /// <returns>잘못된 값일 경우 Default Tile을 반환</returns>
+        private TileData GetStageTileData(RollStage stage, int x, int y)
+        {
+            int tileIndex = stage.GetTile(x, y);
+            if(tileIndex < 0 || tileIndex >= TilePalette.TileData.Count)
+            {
+                Debug.LogError($"TileMap의 TileIndex가 유효하지 않습니다. x: {x}, y: {y}, tileIndex: {tileIndex}");
+                return TilePalette.DefaultTileData;
+            }
+            return TilePalette.TileData[tileIndex];
         }
 
         /// <summary>
@@ -166,42 +186,124 @@ namespace Zephyr.RollGame.Tile
         }
 
         /// <summary>
-        /// Stage의 TileMap에서 해당 좌표의 TileData를 가져온다.
+        /// TileMap에 Tile을 추가한다. 비어 있는 타일을 추가할 경우 아무런 동작을 하지 않는다.
         /// </summary>
-        /// <param name="stage">Stage Data</param>
-        /// <param name="x">Stage X 좌표</param>
-        /// <param name="y">Stage Y 좌표</param>
-        /// <returns>잘못된 값일 경우 Default Tile을 반환</returns>
-        private TileData GetStageTileData(RollStage stage, int x, int y)
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="tileGO">추가할 Tile Gameobject</param>
+        /// <param name="tileData">Tile의 데이터, 현재는 TileData를 Tile GameObject가 공유해서 쓰고 있다.</param>
+        private void AddTile(int x, int y, GameObject tileGO, TileData tileData)
         {
-            int tileIndex = stage.GetTile(x, y);
-            if(tileIndex < 0 || tileIndex >= TilePalette.TileData.Count)
+            if(tileGO == null)
             {
-                Debug.LogError($"TileMap의 TileIndex가 유효하지 않습니다. x: {x}, y: {y}, tileIndex: {tileIndex}");
-                return TilePalette.DefaultTileData;
+                return;
             }
-            return TilePalette.TileData[tileIndex];
+            // tiles는 이미 null로 되어 있으므로 비어 있는 타일을 처리할 필요가 없다.
+            _tiles[x, y] = tileGO;
+            _tileDataMap.Add(tileGO, tileData);
         }
-
+        
         /// <summary>
         /// TileMap에서 해당 좌표의 Tile 데이터를 가져온다. 좌하단이 (0, 0), (x, y) 좌표계
         /// </summary>
         /// <param name="x">x 좌표</param>
         /// <param name="y">y 좌표</param>
-        /// <returns>out of boundary</returns>
+        /// <returns>Tile이 없을 경우 empty를 반환, 범위를 벗어났을 경우 null을 반환</returns>
         public TileData GetTileData(int x, int y)
         {
-            int lengthX = _tiles.GetLength(0);
-            int lengthY = _tiles.GetLength(1);
-            if(x < 0 || x >= lengthX || y < 0 || y >= lengthY)
+            if(!IsInTileMap(x, y))
             {
                 return null;
             }
-            if(_tiles[x, y] == null)
+            var tile = GetTile(x, y);
+            if(tile == null)
             {
                 return new TileData() { IsEmpty = true };
             }
-            return _tileDataMap[_tiles[x, y]];
+            return _tileDataMap[tile];
+        }
+
+        /// <summary>
+        /// TileMap에서 해당 좌표의 Tile을 가져온다. 좌하단이 (0, 0), (x, y) 좌표계
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>(x,y)의 Tile 객체를 반환한다. 만약에 범위를 벗어났을 경우 null을 반환한다.</returns>
+        public GameObject GetTile(int x, int y)
+        {
+            if(!IsInTileMap(x, y))
+            {
+                return null;
+            }
+            return _tiles[x, y];
+        }
+
+        private (int, int) FindTile(GameObject tile)
+        {
+            for(int y = 0; y < Height; ++y)
+            {
+                for(int x = 0; x < Width; ++x)
+                {
+                    if(_tiles[x, y] == tile)
+                    {
+                        return (x, y);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
+
+        /// <summary>
+        /// TileMap에서 해당 좌표의 Tile을 설정한다. 좌하단이 (0, 0), (x, y) 좌표계, 비어 있는 타일을 설정할려면 null로 설정한다.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="tile"></param>
+        /// <returns>Tile을 설정했을 경우 true를 반환한다. 만약에 범위를 벗어났을 경우 false를 반환한다.</returns>
+        private bool SetTile(int x, int y, GameObject tile)
+        {
+            if(!IsInTileMap(x, y))
+            {
+                return false;
+            }
+            // 이미 해당 좌표에 Tile이 있을 경우 false를 반환한다.
+            if(GetTile(x,y) != null)
+            {
+                return false;
+            }
+            _tiles[x, y] = tile;
+            AdjustTilePos(x, y, tile);
+            return true;
+        }
+
+        private void AdjustTilePos(int x, int y, GameObject tile)
+        {
+            if(!IsInTileMap(x, y))
+            {
+                return;
+            }
+            if(tile == null)
+            {
+                return;
+            }
+            tile.transform.localPosition = new Vector3(_startX + x, _startY + y, tile.transform.localPosition.z);
+        }
+
+        public bool MoveToTile(int x, int y, GameObject tile)
+        {
+            if(!IsInTileMap(x, y))
+            {
+                return false;
+            }
+            // 이미 해당 좌표에 Tile이 있을 경우 false를 반환한다.
+            if(GetTile(x,y) != null)
+            {
+                return false;
+            }
+            var (prevX, prevY) = FindTile(tile);
+            SetTile(prevX, prevY, null);
+            SetTile(x, y, tile);
+            return true;
         }
     }
 }
